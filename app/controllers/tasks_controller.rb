609c1e2cc4a1e51057d809d196919e7f0f822b70
Wendela Lövgren <wendela.lovgren@gmail.com>
2019-08-06 16:43:39 +0200

    task manager functionality working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,29 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.create(params[:task])
+    redirect_to task_path(task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # read all
   get 'tasks', to: 'tasks#index'
-  get "tasks/:id", to: "tasks#show"
+
+  # create
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+
+  # read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # edit
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
